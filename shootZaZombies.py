0210ded09274f8175a2dc877342e45f5910b1426
Gaedar <152930029+Gaedar@users.noreply.github.com>
2023-12-04 18:13:24 -0500

    Update shootZaZombies.py

--- shootZaZombies.py
+++ shootZaZombies.py
@@ -1,25 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 27 01:12:02 2023
+import pygame, simpleGE, random, sys
 
-@author: theaw
-"""
-
-import pygame, simpleGE, random
 pygame.init()
 
 class LblWaves(simpleGE.Label):
     def __init__(self, scene):
         super().__init__()
-        self.center = (550,20)
+        self.center = (550, 20)
         self.waves = 0
         self.text = "WAVE: 0"
         self.hide()
-    
+
     def reset(self):
-        self.lives = 0
+        self.waves = 0
         self.text = f"WAVE: {self.waves}"
-        
+
 class StartScreen(simpleGE.MultiLabel):
     def __init__(self, scene):
         super().__init__()
@@ -31,10 +25,10 @@ class StartScreen(simpleGE.MultiLabel):
             "No Matter What Happens",
             "SURVIVE",
             "Click The Screen to continue",
-            ]
+        ]
         self.center = ((320, 240))
         self.size = ((350, 400))
-       
+
 class Bullet(simpleGE.SuperSprite):
     def __init__(self, scene, parent):
         super().__init__(scene)
@@ -59,26 +53,24 @@ class Player(simpleGE.SuperSprite):
         self.imageMaster = self.images["pistol"]
         self.setAngle(90)
         self.setSize(50, 50)
-        self.forward = 0
-        self.dx = 0
-
+        
     def checkEvents(self):
         if self.scene.isKeyPressed(pygame.K_w):
             self.y += -3
-           
+
         if self.scene.isKeyPressed(pygame.K_s):
             self.y += 3
-       
+
         if self.scene.isKeyPressed(pygame.K_a):
             self.x += -3
-       
+
         if self.scene.isKeyPressed(pygame.K_d):
             self.x += 3
-           
+
         pos = pygame.mouse.get_pos()
         direction_to_mouse = self.dirTo(pos)
         self.setAngle(direction_to_mouse)
-       
+
     def reset(self):
         self.x = 325
         self.y = 250
@@ -86,92 +78,102 @@ class Player(simpleGE.SuperSprite):
     def update(self):
         super().update()
 
-        self.x += self.dx
-        self.y += self.forward
-   
 class Zombie(simpleGE.SuperSprite):
-    def __initad__(self, scene):
-        super().__init(scene)
+    def __init__(self, scene):
+        super().__init__(scene)
         self.images = {
             "walk": pygame.image.load("topdownZomb.png")
-            }
+        }
         self.imageMaster = self.images["walk"]
-        self.setAngel(90)
+        self.setAngle(0)
         self.setSize(50, 50)
-        self.forward = 0
-        self.dx = 0
-       
+        self.reset()
+
     def checkEvents(self):
-        if self.collidesWith(self.scene.bullet):
-            self.reset()
+        #set angle towards player
+        dirToPlayer = self.dirTo(self.scene.player.rect.center)
+        self.setAngle(dirToPlayer)
+        self.setSpeed(2)
+        pass
+
+    def reset(self):
+        side = random.randint(0,3)
+        if side == 0:
+            #top            
+            self.x = random.randint(0, self.screen.get_width())
+            self.y = 0
+        elif side == 1:
+            #left
+            self.x = 0
+            self.y = random.randint(0, self.screen.get_height())
+        elif side == 2:
+            #bottom
+            self.x = random.randint(0, self.screen.get_width())
+            self.y = 480
+        else:
+            #right
+            self.x = 640
+            self.y = random.randint(0, self.screen.get_height())
             
-    
-   
-    def doEvents(self):
-        self.setAngle(Player)
-        self.forward = 5
-    
-       
+            
+
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.startScreen = StartScreen(self)
         self.player = Player(self)
-        self.waves = LblWaves(self)
+        self.waves_label = LblWaves(self)
         self.NUM_BULLETS = 100
-        self.currentBullet = 0    
+        self.currentBullet = 0
+        self.andyZombie = Zombie(self)
+        self.zombie = 0
         self.zombies = []
         self.bullets = []
         for i in range(self.NUM_BULLETS):
             self.bullets.append(Bullet(self, self.player))
-       
-        self.sprites = [self.player, self.startScreen, self.bullets, self.waves]
-        
-        self.zombies =[]
-       
-        self.player.hide()
-        
-    def waves(waveNumber, self):
-        global normalEnemy,speedyEnemy, tankEnemy, enemiesList, waveCount
-        if waveNumber == 1:
-            zombie = 5
-            boss = 0
-            for i in range(zombie):
-                self.zombies.append(Zombie(self, self.player))
 
-        if waveNumber == 2:
-            zombie = 7
-            boss = 0
-       
+        self.sprites = [self.player, self.startScreen, self.bullets, 
+                        self.waves_label, self.zombies, self.andyZombie]
+
+    def spawnWave(self):
+        if self.waves_label.waves == 1:
+            self.zombie = 1
+
+            for i in range(self.zombie):
+                self.zombies.append(Zombie(self))
+            
+            for zombie in self.zombies:
+                zombie.reset()
+               
+
     def doEvents(self, event):
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.currentBullet += 1
-                if self.currentBullet >= self.NUM_BULLETS:
-                    self.currentBullet = 0
-                self.bullets[self.currentBullet].fire()
-     
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.currentBullet += 1
+            if self.currentBullet >= self.NUM_BULLETS:
+                self.currentBullet = 0
+            self.bullets[self.currentBullet].fire()
+
     def resetGame(self):
         self.startScreen.hide()
         self.player.reset()
-        self.waves.show((550, 20))
-       
+        self.waves_label.show((550, 20))
+        self.spawnWave()
+
     def update(self):
         if self.startScreen.clicked:
             self.resetGame()
-        
-        if zombie == 0:
-            if boss == 0:
-                self.scene.lives.lives -= 1
-        
-        self.waves.text = f"WAVES: {self.waves.waves}"
-           
 
+        if self.zombie == 0:
+            self.waves_label.waves += 1
+            self.waves_label.text = f"WAVE: {self.waves_label.waves}"
+            self.spawnWave()
+
+        super().update()
 
 def main():
     game = Game()
     game.start()
-
-   
+    sys.exit()
 
 if __name__ == "__main__":
     main()
